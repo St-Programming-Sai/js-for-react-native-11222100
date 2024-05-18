4949cf8109c43da3c4ca8d510a2b444b0e5c3d48
St-Programming-Sai <inlmensah_lartey@st.ug.edu.gh>
2024-05-18 10:38:48 +0000

    Uploading Task One

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,11 +1,9 @@
 // Task One
 // Function That Squares an item if the item is an even number else the item is tripled.
-function transfromArray(arr){
+function processArray(arr){
     return arr.map((item)=>{
         return (item % 2 === 0) ? (item ** 2) : (item * 3);
     });
 }
 
-
-
-
+console.log(processArray([1, 2, 3, 4]))
